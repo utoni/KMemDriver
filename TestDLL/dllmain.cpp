675c2e3d0292a32b3567002fb269e23c6f07a93b
Toni Uhlig <matzeton@googlemail.com>
2019-09-22 21:56:13 +0200

    removed invalid condition

--- TestDLL/dllmain.cpp
+++ TestDLL/dllmain.cpp
@@ -10,7 +10,7 @@ void APIENTRY LibEntry(void)
 		MessageBoxA(NULL,
 			"DllMain from TestDLL",
 			"TestDLL Notification",
-			MB_OK | MB_ICONINFORMATION) == IDOK ? TRUE : FALSE;
+			MB_OK | MB_ICONINFORMATION);
 	}
 }
 
