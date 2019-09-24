ed0e93a3f863eb21cf7bc1c43ce529a7f8a82aec
Toni Uhlig <matzeton@googlemail.com>
2019-09-24 21:10:13 +0200

    removed threaded version of LibEntry which will not work anyway w/o crt init

--- TestDLL/dllmain.cpp
+++ TestDLL/dllmain.cpp
@@ -13,12 +13,3 @@ void APIENTRY LibEntry(void)
 			MB_OK | MB_ICONINFORMATION);
 	}
 }
-
-extern "C" __declspec(dllexport)
-DWORD WINAPI LibEntryThreaded(_In_ LPVOID lpParameter)
-{
-	LibEntry();
-
-	ExitThread(0);
-	return 0;
-}
