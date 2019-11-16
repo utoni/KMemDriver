d58017f8f12837e1f870f77dacd894ea0549c60c
Toni Uhlig <matzeton@googlemail.com>
2019-11-16 21:14:59 +0100

    improved integration tests by introducing KM_ASSERT_EQUAL macro

--- IntegrationTest/IntegrationTest.cpp
+++ IntegrationTest/IntegrationTest.cpp
@@ -6,33 +6,32 @@
 
 #include <iostream>
 
+#define PRINT_CHECK_MSG(message) std::wcout << L ## message
+#define PRINT_FAIL_MSG() std::wcout << L" [FAIL]" << std::endl
+#define PRINT_OK_MSG() std::wcout << L" [ OK ]" << std::endl
+#define KM_ASSERT_EQUAL(equal, condition, message) \
+	do { PRINT_CHECK_MSG(message); if (condition != equal) { \
+		PRINT_FAIL_MSG(); goto error; } else { PRINT_OK_MSG(); } \
+	} while (0);
+
 int main()
 {
-	std::cout << "IntegrationTest\n";
+	KM_ASSERT_EQUAL(true, true, "Integration Test Init");
 
 	try {
 		KInterface &ki = KInterface::getInstance();
-		if (!ki.Init()) {
-			std::wcout << L"Kernel Interface Init() failed" << std::endl;
-			goto error;
-		}
-		if (!ki.Handshake()) {
-			std::wcout << L"Kernel Interface Handshake() failed" << std::endl;
-			goto error;
-		}
-		if (ki.RecvWait() == SRR_TIMEOUT) {
-			std::wcout << L"Ping -> ";
-			if (!ki.Ping()) {
-				std::wcout << L"Got no valid PONG, abort!" << std::endl;
-			}
-			else std::wcout << L"PONG!" << std::endl;
-		}
-		std::wcout << L"Driver shutdown .." << std::endl;
-		ki.Exit();
+		KM_ASSERT_EQUAL(true, ki.Init(), "Kernel Interface Init");
+		KM_ASSERT_EQUAL(true, ki.Handshake(), "Kernel Interface Handshake");
+		KM_ASSERT_EQUAL(SRR_TIMEOUT, ki.RecvWait(), "Kernel Interface Receive Wait");
+		KM_ASSERT_EQUAL(true, ki.Ping(), "Kernel Interface PING - PONG");
+
+		KM_ASSERT_EQUAL(true, ki.Exit(), "Kernel Interface Driver Shutdown");
 	}
 	catch (std::runtime_error& err) {
 		std::wcout << err.what() << std::endl;
 	}
+
+	std::wcout << "Done." << std::endl;
 error:
 	Sleep(3000);
 }
